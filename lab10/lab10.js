28eeea2ffee75a85b1c9485f18c41314e9d2fcc1
GongoGitHub <samingraffia2003@gmail.com>
2024-06-09 22:02:27 -0700

    LOTS OF UPDATES

--- lab10/lab10.js
+++ lab10/lab10.js
@@ -1,4 +1,23 @@
-/*
-* Author: Sam Ingraffia
-*/
+// Lab 10 - JavaScript for the Web
+// Author: Sam Ingraffia
+// June 9 2024
 
+// Function to generate random text
+function generateRandomText() {
+    const text = ["Purple", "Blue", "Magenta", "Green", "Orange", "Red", "Lavender", "White", "Beige", "Amber",]
+    const min = 1;
+    const max = 1;
+    const randLen = Math.floor(Math.random() * (max − min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+}
+
+// Click listener for button
+$("#make-convo").click(function() {
+    // Get new fake dialogue
+    const newText = generateRandomText();
+    // Replace the content of the output div with new text
+    $("#output").html('<div class="text"><p>' + newText + '</p></div>');
+});
