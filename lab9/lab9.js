ca8623d9630032332ded4b55380a2794727cb2d5
GongoGitHub <samingraffia2003@gmail.com>
2024-06-09 20:42:13 -0700

    lab 9

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -2,3 +2,32 @@
 * Author: Sam Ingraffia
 */
 
+// Functions
+$(document).ready(function() {
+// Add button to sections
+$("#minor-section").append("<button id='blue-button'>Click Here</button>");
+$("#problems").append("<button id='green-button'>Click Here</button>");
+$("#reflection").append("<button id='purple-button'>Click Here</button>");
+$("#results").append("<button id='red-button'>Click Here</button>");
+
+    // Color change button for Challenge Section
+    $("#blue-button").click(function() {
+        $("#challenge-minor-section").toggleClass("challengespecial");
+    })
+    
+    // Color change button for Problems Section
+    $("#green-button").click(function() {
+        $("#problems").toggleClass("problems special");
+    })
+    
+    // Color change button for Reflection Section
+    $("#purple-button").click(function() {
+        $("#reflection").toggleClass("reflectionspecial");
+    })
+    
+    // Color change button for Results Section
+    $("#red-button").click(function() {
+        $("#results").toggleClass("resultsspecial");
+    })
+
+    })
