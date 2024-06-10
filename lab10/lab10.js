64d2f7971047c8a06a363784b1b944339b76420e
GongoGitHub <samingraffia2003@gmail.com>
2024-06-09 22:07:56 -0700

    chanes

--- lab10/lab10.js
+++ lab10/lab10.js
@@ -7,7 +7,7 @@ function generateRandomText() {
     const text = ["Purple", "Blue", "Magenta", "Green", "Orange", "Red", "Lavender", "White", "Beige", "Amber",]
     const min = 1;
     const max = 1;
-    const randLen = Math.floor(Math.random() * (max − min + 1)) + min;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
     // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
     // Generate the random Lorem Ipsum-like text
